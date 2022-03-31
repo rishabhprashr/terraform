dc6abe7a3aa105afd5e81e5714e06866d6739095
Rishabh Sharma <rishabhprashr@gmail.com>
2022-03-31 13:52:02 +0530

    Update main.tf

--- ecs/main.tf
+++ ecs/main.tf
@@ -4,5 +4,38 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "doker_ecr_repo" {
-    name = "doker_ecr_repo"
+    name = "docker_ecr_repo"
+}
+
+resource "aws_ecr_repository_policy" "demo-repo-policy" {
+  repository = aws_ecr_repository.docker_ecr_repo
+  policy     = <<EOF
+  {
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid": "adds full ecr access to the demo repository",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetLifecyclePolicy",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+module "ecr_mirror" {
+    source = "TechToSpeech/ecr-mirror/aws"
+    docker_source ="rishabhprashr:tmdb"
+    ecr_repo_name ="docker_ecr_repo"
+    ecr_repo_tag ="base"
 }
