dbbae5673c7892e8341b76065dba459079730f44
rishabh <rishabhprashr@gmail.com>
2022-04-01 03:17:22 +0530

    .

--- service/main.tf
+++ service/main.tf
@@ -18,8 +18,8 @@ resource "aws_default_subnet" "default_subnet_c" {
 
 resource "aws_ecs_service" "my_first_service" {
   name            = "my-first-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
-  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  cluster         = "${aws_ecs_cluster.docker_ecr_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.deployment_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
 
