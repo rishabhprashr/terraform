bb1bf48020e4f59911e6076cd375d3764a5b247f
rishabh <rishabhprashr@gmail.com>
2022-04-01 09:48:02 +0530

    additional changes

--- task/main.tf
+++ task/main.tf
@@ -45,4 +45,3 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-view rawmain.tf hosted with ❤ by GitHub
