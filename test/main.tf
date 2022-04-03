fa27bd9c325e05a579892eec2a4122a55a60ea88
rishabh <rishabhprashr@gmail.com>
2022-04-03 22:34:51 +0530

    additional change

--- test/main.tf
+++ test/main.tf
@@ -77,6 +77,28 @@ resource "aws_default_subnet" "default_subnet_c" {
 
 
 
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
 
 
 resource "aws_ecs_service" "deployment_service" {
