1869e4ef85024d704614e4ae8fb1c3c79a0ba920
rishabh <rishabhprashr@gmail.com>
2022-04-04 18:01:01 +0530

    additional change

--- test/main.tf
+++ test/main.tf
@@ -47,6 +47,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
+      Service     = "ecs.amazonaws.com"
     }
   }
 }
