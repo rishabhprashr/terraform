9ae58a8de5456c3e864b3c1f4c870f1335d05f09
rishabh <rishabhprashr@gmail.com>
2022-04-11 22:09:46 +0530

    additional change

--- service/main.tf
+++ service/main.tf
@@ -1,18 +1,17 @@
 provider "aws" {
-    version = "~> 2.0"
-    region = "ap-south-1"
+    region = "eu-central-1"
 }
 
 
-resource "aws_ecs_service" "my_first_service" {
-  name            = "my-first-service"                             # Naming our first service
-  cluster         = "arn:aws:ecs:ap-south-1:156927083468:cluster/docker_ecr_cluster"            # Referencing our created Cluster
-  task_definition = "deployment_task" # Referencing the task our service will spin up
+resource "aws_ecs_service" "deployment_service" {
+  name            = "deployment-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.docker_ecr_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.deployment_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
-
-  network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
-    assign_public_ip = true # Providing our containers with public IPs
-  }
+  desired_count   = 2
+  force_new_deployment = true # Setting the number of containers to 3
+  depends_on      = [
+      aws_lb_listener.listener,
+      aws_iam_role_policy_attachment.ecsTaskExecutionRole_policy
+      ]
 }
